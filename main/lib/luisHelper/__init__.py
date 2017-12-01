31e023865851f8c2c698ae34baa6e6312c2ce05a
Chirag S <pears.chirag@gmail.com>
2017-12-01 12:30:20 +0530

    test main function

--- main/lib/luisHelper/__init__.py
+++ main/lib/luisHelper/__init__.py
@@ -31,4 +31,4 @@ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
-# from .luisHelper import getIntentAndEntities
+from .luisHelper import getIntentAndEntities
